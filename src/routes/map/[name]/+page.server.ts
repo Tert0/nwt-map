b6be84cd23fee9d7eeb04ededa9936c64106b45c
Tert0 <62036464+Tert0@users.noreply.github.com>
2024-01-04 18:43:18 +0100

    backend: switched from node crypto to web crypto api (cloudflare pages does not support node crypto)

--- src/routes/map/[name]/+page.server.ts
+++ src/routes/map/[name]/+page.server.ts
@@ -1,15 +1,16 @@
 import type { PageServerLoad } from './$types';
-import { createHash } from 'crypto';
 import { env } from '$env/dynamic/private';
 
 const RAW_DATA = JSON.parse(env.DATA || "[]") as string[];
 
-const DATA = Object.fromEntries(RAW_DATA.map(encoded_entry => {
-    return [createHash('sha256').update(Uint8Array.from(atob(encoded_entry), c => c.charCodeAt(0))).digest("hex"), encoded_entry];
-}));
-
 export const load: PageServerLoad = async ({ params }) => {
-	return {
-		data: DATA[params.name]
-	};
+    for(const encoded_entry of RAW_DATA) {
+        const hash = Array.from(new Uint8Array(await crypto.subtle.digest("SHA-256", Uint8Array.from(atob(encoded_entry), c => c.charCodeAt(0))))).map((b) => b.toString(16).padStart(2, "0"))
+        .join("");
+        if(params.name == hash) {
+            return {
+                data: encoded_entry
+            }
+        }
+    }
 };
